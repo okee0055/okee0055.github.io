606b95b0e62e0af74e4dd421c5b0e7cbfe2dfe4c
faul7249 <30452510+faul7249@users.noreply.github.com>
2018-04-16 13:21:38 -0500

    Updates

---
+++ Assignment3/server.js
@@ -0,0 +1,58 @@
+var path = require('path');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var multiparty = require('multiparty');
+
+var mime = require('./src/mime.js');
+
+var port = 8002;
+var public_dir = path.join(__dirname, 'public');
+
+
+var server = http.createServer((req, res) => {
+    var req_url = url.parse(req.url);
+    var filename = req_url.pathname.substring(1);
+
+    if(filename === '') filename = 'index.html';
+
+    if(req.method === 'GET') {
+        fs.readFile(path.join(public_dir, filename), (err, data) => {
+            if(err)
+            {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.write('Oh no! Couldn\'t find that page!');
+                res.end();
+            }
+            else{
+                var ext = path.extname(filename).substring(1);
+                console.log(' serving file ' + filename + ' (type = ' + mime.mime_types[ext] + ')');
+                console.log(' used mime module version: ' + mime.version);
+                res.writeHead(200, {'Content-Type': mime.mime_types[ext] || 'text/plain'});
+                res.write(data);
+                res.end();
+            }
+
+        });
+
+    }
+
+    else if(req.method === 'POST'){
+        if(filename === 'subscribe'){
+            var form = new multiparty.Form();
+            form.parse(req, (err, fields, files) =>  {
+                console.log(fields);
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.write('Successfully subscribed!');
+                res.end();
+            });//formparse
+          }//subscribe
+        }//post request
+      });
+    console.log(' now listening at port ' + port);
+
+    server.listen(port, '0.0.0.0');
+
+
+                                                  57,1          35%
